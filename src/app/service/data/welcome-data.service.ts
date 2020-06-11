389b8d298bfe05c8df47a6aa55e3491f5ba16fd4
Nelson Miranda <nelsonmiranda8421@gmail.com>
2020-06-11 00:16:51 -0600

    Welcome service added

---
+++ src/app/service/data/welcome-data.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export class Welcome{
+  constructor(
+    message : string
+  ){}
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WelcomeDataService {
+
+  constructor(
+    private httCliente : HttpClient
+  ) { }
+
+  getWelcomeMessage() {
+    return this.httCliente.get<Welcome>('http://localhost:8080/welcome-message');
+  }
+}
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap, Params } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 // import {AppComponent} from '../app.component';
 
 @Component({
@@ -11,15 +12,30 @@ export class WelcomeComponent implements OnInit {
 
   message = 'Welcome !!!';
   name = '';
+  welcomeMessage : string
 
   constructor(
-    private route : ActivatedRoute,
-  ) {}
+    private route: ActivatedRoute,
+    private welcomeService: WelcomeDataService
+  ) { }
 
   ngOnInit(): void {
     this.name = this.route.snapshot.params['name'];
     // this.name = this.route.snapshot.params['name']
-    console.log(this.route.snapshot.params['name']);
+    //console.log(this.route.snapshot.params['name']);
+  }
+
+  getWelcomeFromService() {
+    this.welcomeService.getWelcomeMessage().subscribe(
+      response => this.handleSuccessfulResponse(response)
+    );
+    // console.log("Last line of the welcome message");
+  }
+
+  handleSuccessfulResponse(response){
+    // console.log(response);
+    this.welcomeMessage = response.message;
+    console.log(response.message);
   }
 
 }
