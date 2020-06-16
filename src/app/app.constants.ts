a7c78b34298e1806d0ae59b92e98cbc8d9a49d16
Nelson Miranda <nelsonmiranda8421@gmail.com>
2020-06-15 22:29:12 -0600

    Connecting RESTful API with JPA and Hibernate

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,3 +1,4 @@
 import { constants } from "buffer";
 
 export const API_URL = 'http://localhost:8080';
+export const API_JPA_URL = 'http://localhost:8080/jpa';
--- src/app/service/data/todo.service.ts
+++ src/app/service/data/todo.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Todo } from 'src/app/list-todos/list-todos.component';
-import { API_URL } from 'src/app/app.constants';
+import { API_URL, API_JPA_URL } from 'src/app/app.constants';
 
 @Injectable({
   providedIn: 'root'
@@ -13,27 +13,27 @@ export class TodoService {
   ) { }
 
   retrieveAllTodos(username: string) {
-    return this.http.get<Todo[]>(`${API_URL}/users/${username}/todos`);
+    return this.http.get<Todo[]>(`${API_JPA_URL}/users/${username}/todos`);
   }
 
   getTodo(username: string, id : number) {
-    return this.http.get<Todo>(`${API_URL}/users/${username}/todos/${id}`);
+    return this.http.get<Todo>(`${API_JPA_URL}/users/${username}/todos/${id}`);
   }
 
   createTodo(username: string, todo : Todo) {
     return this.http.post(
-      `${API_URL}/users/${username}/todos`,
+      `${API_JPA_URL}/users/${username}/todos`,
        todo);
   }
 
   updateTodo(username: string, id : number, todo : Todo) {
     return this.http.put(
-      `${API_URL}/users/${username}/todos/${id}`,
+      `${API_JPA_URL}/users/${username}/todos/${id}`,
        todo);
   }
 
   deleteTodo(username: string, id : number) {
-    return this.http.delete(`${API_URL}/users/${username}/todos/${id}`);
+    return this.http.delete(`${API_JPA_URL}/users/${username}/todos/${id}`);
   }
 
 }
